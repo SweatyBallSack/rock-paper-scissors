c0e082edce683a35fe13a14f56c8156129c697a2
Joseph Lin <149325305+SweatyBallSack@users.noreply.github.com>
2024-02-05 15:28:48 -0500

    Fixed error that returned wrong result e.g. You Win! rock beats paper as a result of wrong variable name.

--- script.js
+++ script.js
@@ -13,10 +13,56 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
+function playRound(playerSelection,computerSelection) {
+    let playerChoice = prompt("Enter your choice (rock/paper/scissors)").toLowerCase();
+    let playerValue;
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    let computerValue;
+    if(playerChoice === "rock") {
+        playerValue = 1;
+    }
+    else if(playerChoice === "paper") {
+        playerValue = 2;
+    }
+    else if(playerChoice === "scissors") {
+        playerValue = 3;
+    }
 
+    if(computerChoice === "rock") {
+        computerValue = 1;
+    }
+    else if(computerChoice === "paper") {
+        computerValue = 2;
+    }
+    else if(computerChoice === "scissors") {
+        computerValue = 3;
+    }
 
+    if(playerValue === computerValue) {
+        return "You Drew! " + playerChoice + " draws with " + computerChoice;
+    }
+    else if (playerValue === 1 && computerValue === 2 || playerValue === 2 && computerValue === 3 
+        || playerValue === 3 && computerValue === 1) {
+        return "You Lose! " + computerChoice + " beats " + playerChoice;
+    }
+    else {
+        return "You Win! " + playerChoice + " beats " + computerChoice;
+    }
+}
 
+console.log(playRound());
+
+function playGame() {
+    let playerScore;
+    let computerScore;
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
 
 
